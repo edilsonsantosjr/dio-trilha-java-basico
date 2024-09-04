7e9d1e69f0f6987f8ed43c952a35658e07a48f7e
Edilson Santos <edilson0982021@gmail.com>
2024-09-04 16:26:16 -0300

    Project done

---
+++ src/main/java/org/example/ContaTerminal.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        //TODO:
+
+        //Conhecer e importar a classe Scanner
+
+        //Obter pela scanner os valores digitados
+
+        //Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da sua conta!");
+        int number = scanner.nextInt();
+
+        System.out.println("Digite a agencia!");
+        String agency = scanner.next();
+
+        System.out.println("Digite seu nome!");
+        String name = scanner.next();
+
+        System.out.println("Digite seu saldo!");
+        Double balance = scanner.nextDouble();
+
+        System.out.println("Olá ".concat(name)+ ", obrigado por criar uma conta em nosso banco, sua agência é ".concat(agency) +", conta " + "e seu saldo " + balance + " já está disponível para saque.");
+
+    }
+
+}
